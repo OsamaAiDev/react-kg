bd2f38f80e17d2de7d60a4005aa430c11505d7e1
Osama Arshad <osamafreelacer22@gmail.com>
2025-10-25 18:59:58 +0500

    started todo v1 project

---
+++ 01_usingBootstrap/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+    </>
+  );
+}
+
+export default App;
---
+++ projects/01_todo/src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div className="todo-container">
+      <h1>TODO App</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-8">col-8</div>
+          <div class="col-4">col-4</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
